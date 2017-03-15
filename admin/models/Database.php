4b9211f34f333e03d85ee20184e32491cfdc033b
Goferberg Igor <igorgof@yandex.ru>
2017-03-15 14:51:33 +0500

    Admin
    Добавлен класс подключение к БД

---
+++ admin/models/Database.php
@@ -0,0 +1,9 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $user = "root";
+        private $pass = "cbcflvby235";
+        private $db = "pz";
+        
+    }
+?> 
