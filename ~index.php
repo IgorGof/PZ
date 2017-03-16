484487ed14b592bf4f08df8030caeed297391508
Goferberg Igor <igorgof@yandex.ru>
2017-03-16 17:49:31 +0500

    Журнал записей
    Работающий вывод журнала

---
+++ ~index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Предварительная запись</title>
+        <link rel="stylesheet" href="css/my.css"/>
+    </head>
+    <body>
+        <div class = "Zagolovok">
+            <span>Журнал предварительной записи</span>
+        </div>
+        <div class = "table">
+            <table>
+                <tr>
+                    <th>№<br />п/п</th>
+                    <th>ФИО<br />(к кому)</th>
+                    <th>День<br />(в какой день недели)</th>
+                    <th>Время<br />(в какое время)</th>
+                    <th>Цель<br />(Дель посещения)</th>
+                    <th>ФИО<br />(ФИО посетителя)</th>
+                    <th>ФИО<br />(запись внес)</th>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
